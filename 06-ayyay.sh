0a2126cd93836c6c3f6e1c2e91dbbea12ec798b3
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-04 11:28:17 -0500

    array

--- 06-ayyay.sh
+++ 06-ayyay.sh
@@ -4,10 +4,10 @@
 
 FRUITS=("APPLE" "KIWI" "BANANA") #array
 
-echo "first fruit is: $fruits[0]"
-echo "second fruit is: $fruits[1]"
-echo "third fruit is: $fruits[2]"
+echo "first fruit is: ${fruits[0]}"
+echo "second fruit is: ${fruits[1]}"
+echo "third fruit is: ${fruits[2]}"
 
-echo "all fruits are: $fruits[@]"
+echo "all fruits are: ${fruits[@]}"
 
 
