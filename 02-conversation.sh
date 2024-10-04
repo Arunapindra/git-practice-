84a70301374bd0a57ceb9ca0d2663f288251ca9a
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-03 23:25:58 -0500

    conversation btw ramesh and suresh

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this symbol used to comment
+echo "ramesh=hi suresh! how are you?"
+echo "suresh=hi iam doing good ramesh."
+echo "ramesh=how is everything going suresh!"
+echo "suresh=everything is going good ramesh."
