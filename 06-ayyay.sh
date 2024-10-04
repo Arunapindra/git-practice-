bdf8a980b7353ca410ef368f564c9cbe9858aa4f
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-04 11:25:23 -0500

    array

---
+++ 06-ayyay.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#index starts from 0, size is 3
+
+FRUITS=("APPLE" "KIWI" "BANANA") #array
+
+echo "first fruit is: $fruits[0]"
+echo "second fruit is: $fruits[1]"
+echo "third fruit is: $fruits[2]"
+
+echo "all fruits are: $fruits[@]"
+
+
