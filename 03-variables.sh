c4d5bc2985c81462dcbb97fe145f973a891692b4
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-03 23:36:45 -0500

    added $ for direct variables calling

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1=hi $person2! how are you?"
+echo "$person2=hi iam doing good $person1."
+echo "$person1=how is everything going $person2!"
+echo "$person2=everything is going good $person1."
