9cd2bac4ab35ad866a75af1dc92164dbe73d2e42
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-05 17:33:51 -0500

    functions

--- 13-colours.sh
+++ 13-colours.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo -e "\e[31m HELLO WORLD"
+echo -e "\e[31m HELLO WORLD" # adding red color to the script
+echo -e "\e[32m HELLO WORLD" # adding green color to the script
+echo -e "\e[33m HELLO WORLD" # adding yellow color to the script
