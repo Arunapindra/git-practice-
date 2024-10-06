d58a17dd84bb04134c759c45c7446938c1779004
Arunapindra <150082622+Arunapindra@users.noreply.github.com>
2024-10-06 11:37:29 -0500

    functions

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
-echo "7"
+for i in { 1..10 }
+do
+    echo "$i"
+done
+
+
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
